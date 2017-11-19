42aefca2dc52c4b20581851f01d07886185450b3
Vegard Valvik <vegard.valvik@gmail.com>
2017-11-19 16:09:08 +0100

    File rename

--- json.py
+++
@@ -1,6 +0,0 @@
-curl	-s -N 'Accept: application/vnd.twitchtv.v5+json' \
-   		-H 'Client-ID: dsv0rf69bvzgi9ch6ys16vwncjax1z' \
-		-H 'Authorization: OAuth ***' \
-		-X GET 'https://api.twitch.tv/kraken/streams/followed' |
-
-		python -m script
---
+++ live.py
@@ -0,0 +1,5 @@
+curl	-H 'Accept: application/vnd.twitchtv.v5+json' \
+   		-H 'Client-ID: dsv0rf69bvzgi9ch6ys16vwncjax1z' \
+		-H 'Authorization: OAuth ***' \
+		-X GET 'https://api.twitch.tv/kraken/streams/followed' \
+		| python -m script
