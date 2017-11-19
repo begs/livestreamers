310535db76d14ca5514052eae9a26c26984adcfe
Vegard Valvik <vegard.valvik@gmail.com>
2017-11-19 16:18:51 +0100

    Muted curl

--- live.py
+++ live.py
@@ -1,4 +1,4 @@
-curl	-H 'Accept: application/vnd.twitchtv.v5+json' \
+curl	-s 'Accept: application/vnd.twitchtv.v5+json' \
    		-H 'Client-ID: dsv0rf69bvzgi9ch6ys16vwncjax1z' \
 		-H 'Authorization: OAuth ***' \
 		-X GET 'https://api.twitch.tv/kraken/streams/followed' \
