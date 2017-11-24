3d3686827bc8721d3c631711d7ab9f4f5ef46f25
Vegard Valvik <vegard.valvik@gmail.com>
2017-11-24 15:09:11 +0100

    Added check for vodcast

--- script.py
+++ script.py
@@ -8,6 +8,9 @@ class formatting:
 	BOLD = "\033[1m"
 	ENDC = "\033[0m"
 	OKGREEN = "\033[92m"
+	RED = "\033[91m"
+	DEFAULT = "\033[99m"
+
 
 for i in range (0, numStreams): 
 	if (i == 0):
@@ -16,6 +19,7 @@ for i in range (0, numStreams):
 	channelName = data["streams"][i]["channel"]["name"];
 	channelGame = data["streams"][i]["channel"]["game"];
 	channelViewers = str(data["streams"][i]["viewers"]);
+	streamType = data["streams"][i]["stream_type"];
 
 	#Attempt to get output properly tabulated
 	if (len(channelName) < 8):
@@ -31,7 +35,13 @@ for i in range (0, numStreams):
 	if(len(channelGame) > 20):
 		channelGame = channelGame[:20] + ".."
 
-	print formatting.BOLD + channelName + "\t" + formatting.ENDC + channelGame + "\t" + formatting.OKGREEN + channelViewers + formatting.ENDC
+	#Check if stream is actually live or VodCast
+	if(streamType == "live"):
+		streamType = "";
+	else:
+		streamType = "(vodcast)";
+
+	print formatting.BOLD + channelName + "\t" + formatting.ENDC + channelGame + "\t" + formatting.OKGREEN + channelViewers + formatting.RED + "\t" + streamType + formatting.ENDC
 
 	if (i == numStreams-1):
 		print "\n"
