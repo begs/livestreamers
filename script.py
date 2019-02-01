f4691bc36bb5836e660001c10809979f6fb984dd
begs <vegard.valvik@gmail.com>
2019-02-01 13:12:37 +0100

    Fixed compability for Python 2.*, 3.*

--- script.py
+++ script.py
@@ -1,6 +1,11 @@
 import sys, json; 
-reload(sys)
-sys.setdefaultencoding('utf8')
+#Only necessary to set default encoding in Python 2.* - doesn't work on Python 3.*
+try:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+except Exception:
+    pass
+
 data = json.load(sys.stdin); 
 
 #For prettier output formatting
@@ -14,10 +19,10 @@ class formatting:
 try:                                                                         
     numStreams = data["_total"];                                             
 except KeyError:                                                             
-    print formatting.RED + "KeyError - make sure your OAuth is formatted correctly in live.py" + formatting.ENDC
+    print (formatting.RED + "KeyError - make sure your OAuth is formatted correctly in live.py" + formatting.ENDC)
     sys.exit(1)                                                              
                
-print "\nCHANNEL " + ' '*13 + "GAME" + ' '*37 + "VIEWERS" + ' '*8 + "\n" + '-'*80
+print ("\nCHANNEL " + ' '*13 + "GAME" + ' '*37 + "VIEWERS" + ' '*8 + "\n" + '-'*80)
 
 for i in range (0, numStreams): 
 	channelName = data["streams"][i]["channel"]["name"];
@@ -39,12 +44,12 @@ for i in range (0, numStreams):
 		channelGame = channelGame[:38] + ".."
 
 	#Formatting
-	print "{} {} {} {}".format(
+	print ("{} {} {} {}".format(
 		formatting.HEADER + channelName.ljust(20) + formatting.ENDC,
 		channelGame.ljust(40), 
 		formatting.OKGREEN + channelViewers.ljust(8), 
 		formatting.RED + streamType + formatting.ENDC
-		)
+		))
 
 	if (i == numStreams-1):
-		print '-'*80
+		print ('-'*80)
