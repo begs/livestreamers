2c6c36a947764e54c2f84f6550ad60e17b12901c
begs <vegard.valvik@gmail.com>
2019-09-16 16:04:31 +0200

    Request fixed, now allows for Twitch API v5 usage

--- live.py
+++ live.py
@@ -1,5 +1,5 @@
-curl	-s 'Accept: application/vnd.twitchtv.v5+json' \
-   		-H 'Client-ID: dsv0rf69bvzgi9ch6ys16vwncjax1z' \
-		-H 'Authorization: OAuth ***' \
-		-X GET 'https://api.twitch.tv/kraken/streams/followed' \
-		| python -m script
+curl -s -H 'Accept: application/vnd.twitchtv.v5+json' \
+-H 'Client-ID: dsv0rf69bvzgi9ch6ys16vwncjax1z' \
+-H 'Authorization: OAuth ***' \
+-X GET 'https://api.twitch.tv/kraken/streams/followed' \
+| python -m script
