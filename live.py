1f8c213d328e2ca0decc822a52e6b608db2558c0
begs <vegard.valvik@gmail.com>
2020-03-03 09:42:29 +0100

    Read OAuth from input

--- live.py
+++ live.py
@@ -1,56 +1,63 @@
 import sys, json, requests; 
 #Only necessary to set default encoding in Python 2.* - doesn't work on Python 3.*
 try:
-	reload(sys)
-	sys.setdefaultencoding('utf8')
+    reload(sys)
+    sys.setdefaultencoding('utf8')
 except Exception:
     pass
+
+#See if oauth.txt exists
+try:
+    with open('oauth.txt', 'r') as f:
+        oauth = f.read()
+#Store OAuth in oauth.txt
+except FileNotFoundError:
+    print("Paste your OAuth (like 'yaxb50....')")
+    with open('oauth.txt', 'w') as f:
+        f.write (input ())
+    with open('oauth.txt', 'r') as f:
+        oauth = f.read()
 
 headers = {
     'Accept': 'application/vnd.twitchtv.v5+json',
     'Client-ID': 'dsv0rf69bvzgi9ch6ys16vwncjax1z',
-    'Authorization': 'OAuth ***',
+    'Authorization': 'OAuth ' + oauth,
 }
+try:
+    response = requests.get('https://api.twitch.tv/kraken/streams/followed', headers=headers)
+    data = response.json()
+    numStreams = len(data['streams'])
+except (KeyError, ValueError):
+    print("Error - make sure your OAuth is formatted correctly in oauth.txt")
+    sys.exit(1)
 
-response = requests.get('https://api.twitch.tv/kraken/streams/followed', headers=headers)
-data = response.json()
-
-#Try to get stream info from json. Gives KeyError if the OAuth fails         
-#Count number of live streams
-try:                                                                         
-	numStreams = len(data['streams'])                                             
-except KeyError:                                                             
-    	print ("KeyError - make sure your OAuth is formatted correctly in live.py")
-    	sys.exit(1)                                                              
-               
 print ("\nCHANNEL " + ' '*13 + "GAME" + ' '*37 + "VIEWERS" + ' '*8 + "\n" + '-'*80)
 
 for i in range (0, numStreams): 
-	channelName = data["streams"][i]["channel"]["name"];
-	channelGame = data["streams"][i]["channel"]["game"];
-	channelViewers = str(data["streams"][i]["viewers"]);
-	streamType = data["streams"][i]["stream_type"];
+    channelName = data["streams"][i]["channel"]["name"];
+    channelGame = data["streams"][i]["channel"]["game"];
+    channelViewers = str(data["streams"][i]["viewers"]);
+    streamType = data["streams"][i]["stream_type"];
 
-	#Check if stream is actually live or VodCast
-	if(streamType == "live"):
-		streamType = "";
-	else:
-		streamType = "(vodcast)";
+    #Check if stream is actually live or VodCast
+    if(streamType == "live"):
+    	streamType = "";
+    else:
+    	streamType = "(vodcast)";
 
-	#Truncate long channel names/games
-	if(len(channelName) > 18):
-		channelName = channelName[:18] + ".."
+    #Truncate long channel names/games
+    if(len(channelName) > 18):
+    	channelName = channelName[:18] + ".."
+    if(len(channelGame) > 38):
+        channelGame = channelGame[:38] + ".."
 
-	if(len(channelGame) > 38):
-		channelGame = channelGame[:38] + ".."
+    #Formatting
+    print ("{} {} {} {}".format(
+	channelName.ljust(20),
+	channelGame.ljust(40), 
+	channelViewers.ljust(8), 
+	streamType
+    ))
 
-	#Formatting
-	print ("{} {} {} {}".format(
-		channelName.ljust(20),
-		channelGame.ljust(40), 
-		channelViewers.ljust(8), 
-		streamType
-		))
-
-	if (i == numStreams-1):
-		print ('-'*80)
+    if (i == numStreams-1):
+        print ('-'*80)
